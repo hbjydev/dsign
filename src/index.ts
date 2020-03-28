81c5d4df3b38d74137c73cfd9bd6a0c5014fab09
Hayden Young <git@hayden.codes>
2020-03-28 14:22:04 +0000

    refactor: include DefaultTheme export in initial ThemeContext export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { default } from './utils/Dsign';
 export { default as Button } from './components/Button';
-export { default as ThemeContext } from './utils/ThemeContext';
-export { DefaultTheme } from './utils/ThemeContext';
+export { default as ThemeContext, DefaultTheme } from './utils/ThemeContext';
