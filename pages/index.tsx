559612835113a6e52dc36338569ff4b9a359a541
Hayden Young <git@hayden.codes>
2020-04-02 14:57:15 +0100

    docs: create hero segment of the landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,35 @@
-import React from 'react';
-import { Container } from '..';
+import * as React from 'react';
+import { Container, Button } from '..';
 
 export default () => {
-  return (
-    <Container>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-    </Container>
-  );
+    return (
+        <Container>
+            <div style={{ display: 'flex', alignItems: 'center', padding: '2rem 0', borderBottom: '1px solid var(--accent-1)' }}>
+                <div className="left" style={{ width: '100%' }}>
+                    <h1>React UI made easy.</h1>
+                    <p>Dsign is a modern UI framework built for React, designed to help ease your development cycle.</p>
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        width: '100%'
+                    }}>
+                        <Button color="success" filled shadow>
+                            Get Started
+                        </Button>
+                    </div>
+                </div>
+                <div className="right" style={{
+                    width: '100%',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    padding: '2rem'
+                }}>
+                    <img
+                        src="https://assets.zeit.co/image/upload/v1583250191/front/home/new/acme.svg"
+                        style={{ width: '100%' }} />
+                </div>
+            </div>
+        </Container>
+    );
 }
